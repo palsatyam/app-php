90d2cd8c35383f5c422e32b8444122e269fd4603
unknown <em.satyam@gmail.com>
2022-11-08 21:43:19 +0530

    Multi-dimensional Array

---
+++ Lrn-progress/multi-dimensional-array.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = array(
+    $f_name = array("satya", "monu", "sonu"),
+    
+    $l_name = array("singh", "jha", "kumar")
+    );
+
+// echo($a["f_name"][1]. " ". $a["l_name"][1]); 
+echo $a[0][1];
+echo " ";
+echo $a[1][1];
