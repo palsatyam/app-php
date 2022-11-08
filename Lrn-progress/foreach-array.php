76774a7e7ee04879454d90d690163088496d114a
unknown <em.satyam@gmail.com>
2022-11-08 21:42:06 +0530

    Foreach-array

---
+++ Lrn-progress/foreach-array.php
@@ -0,0 +1,12 @@
+<?php
+
+// foreach-array 
+
+
+$bhau = array("monu"=>"12","chintu"=>"13","shyam"=>"15","mintu"=>"10");
+
+foreach($bhau as $x => $x_value){
+echo($x . " " . $x_value);
+
+echo "<br />";
+}
