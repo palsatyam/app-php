05f400c24874a74311be76cc8393a17b005fc9b1
unknown <em.satyam@gmail.com>
2022-11-10 23:45:44 +0530

    File Modified #8487

--- Lrn-progress/string.php
+++ Lrn-progress/string.php
@@ -3,3 +3,11 @@
 
 $a = "That is not a Good Player.";
 echo strlen($a).($a);
+
+// str_word_count() - Count Words in a String 
+
+// That will count the words 
+
+// like this 
+ 
+echo  str_word_count($a);
